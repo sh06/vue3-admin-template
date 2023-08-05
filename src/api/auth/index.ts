9d13ff2d14975b36cff2801fcdf1428f48deb9fe
sh06 <hehe_0108@outlook.com>
2023-08-06 03:06:56 +0800

    feat: 开发环境使用自定义代理

--- src/api/auth/index.ts
+++ src/api/auth/index.ts
@@ -6,7 +6,5 @@ enum API {
 }
 
 export const login = (data: loginForm) => {
-  http.post<string, loginResponseData>(API.LOGIN_URL, data, {
-    baseURL: 'http://127.0.0.1:4523/m1/1186598-0-default/api'
-  })
+  http.post<string, loginResponseData>(API.LOGIN_URL, data)
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import path from 'path'
@@ -13,50 +13,62 @@ import IconsResolver from 'unplugin-icons/resolver'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    AutoImport({
-      imports: ['vue'],
-      resolvers: [
-        ElementPlusResolver(),
-        IconsResolver({
-          prefix: 'Icon'
-        })
-      ],
-      dts: path.resolve(__dirname, 'types/auto-imports.d.ts'),
-      eslintrc: {
-        enabled: false
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  return {
+    plugins: [
+      vue(),
+      vueJsx(),
+      AutoImport({
+        imports: ['vue'],
+        resolvers: [
+          ElementPlusResolver(),
+          IconsResolver({
+            prefix: 'Icon'
+          })
+        ],
+        dts: path.resolve(__dirname, 'types/auto-imports.d.ts'),
+        eslintrc: {
+          enabled: false
+        }
+      }),
+      Components({
+        resolvers: [
+          ElementPlusResolver(),
+          IconsResolver({
+            enabledCollections: ['ep']
+          })
+        ],
+        dts: path.resolve(__dirname, 'types/components.d.ts')
+      }),
+      Icons({
+        autoInstall: true
+      }),
+      createSvgIconsPlugin({
+        iconDirs: [path.resolve(__dirname, 'src/assets/svg')],
+        symbolId: 'icon-[name]'
+      })
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
       }
-    }),
-    Components({
-      resolvers: [
-        ElementPlusResolver(),
-        IconsResolver({
-          enabledCollections: ['ep']
-        })
-      ],
-      dts: path.resolve(__dirname, 'types/components.d.ts')
-    }),
-    Icons({
-      autoInstall: true
-    }),
-    createSvgIconsPlugin({
-      iconDirs: [path.resolve(__dirname, 'src/assets/svg')],
-      symbolId: 'icon-[name]'
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@use "./src/styles/variables.scss" as *;`,
-        javascriptEnabled: true
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "./src/styles/variables.scss" as *;`,
+          javascriptEnabled: true
+        }
+      }
+    },
+    server: {
+      proxy: {
+        [env.VITE_APP_BASE_API]: {
+          target: env.VITE_APP_BASE_URL,
+          changeOrigin: true,
+          rewrite: (url) => url.replace(RegExp(`^${env.VITE_APP_BASE_API}`), '')
+        }
       }
     }
   }
